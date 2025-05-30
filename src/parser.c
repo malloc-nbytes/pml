35b0f89219c9b31354303dc48ac3e4cc449a7daa
malloc-nbytes <zdhdev@yahoo.com>
2025-05-30 18:31:39 -0500

    add parsing context

--- src/parser.c
+++ src/parser.c
@@ -8,7 +8,12 @@
 #include "err.h"
 #include "utils.h"
 
-static Expr *parse_expr(Lexer *l);
+typedef struct {
+        Lexer *l;
+        int global_scope;
+} Parsing_Context;
+
+static Expr *parse_expr(Parsing_Context *ctx);
 
 static Token *expect(Lexer *l, Token_Type exp) {
         Token *hd = lexer_next(l);
@@ -19,8 +24,8 @@ static Token *expect(Lexer *l, Token_Type exp) {
         return hd;
 }
 
-static Token *expectkw(Lexer *l, const char *kw) {
-        Token *hd = lexer_next(l);
+static Token *expectkw(Parsing_Context *ctx, const char *kw) {
+        Token *hd = lexer_next(ctx->l);
         if (hd->ty != TOKEN_TYPE_KEYWORD || strncmp(hd->lx.s, kw, hd->lx.l) != 0) {
                 err_wargs("expected keyword `%s` but got `%s`",
                           kw, utils_tmp_str_wlen(hd->lx.s, hd->lx.l));
@@ -28,45 +33,45 @@ static Token *expectkw(Lexer *l, const char *kw) {
         return hd;
 }
 
-static Expr_Let *parse_expr_let(Lexer *l) {
-        lexer_discard(l); // let
-        Token *id = expect(l, TOKEN_TYPE_IDENTIFIER);
-        (void)expect(l, TOKEN_TYPE_EQUALS);
-        Expr *e = parse_expr(l);
-        (void)expectkw(l, GL_KW_IN);
-        Expr *in = parse_expr(l);
+static Expr_Let *parse_expr_let(Parsing_Context *ctx) {
+        lexer_discard(ctx->l); // let
+        Token *id = expect(ctx->l, TOKEN_TYPE_IDENTIFIER);
+        (void)expect(ctx->l, TOKEN_TYPE_EQUALS);
+        Expr *e = parse_expr(ctx);
+        (void)expectkw(ctx, GL_KW_IN);
+        Expr *in = parse_expr(ctx);
         return expr_let_alloc(id->lx.s, id->lx.l, e, in);
 }
 
-static Expr_Array parse_comma_sep_exprs(Lexer *l) {
+static Expr_Array parse_comma_sep_exprs(Parsing_Context *ctx) {
         assert(0);
 }
 
-static Expr *parse_primary_expr(Lexer *l) {
+static Expr *parse_primary_expr(Parsing_Context *ctx) {
         Token *tok = NULL;
         Expr *left = NULL;
         Expr *right = NULL;
 
         // Unary expressions
-        while (lexer_peek(l, 0) && (lexer_peek(l, 0)->ty == TOKEN_TYPE_MINUS
-                                 || lexer_peek(l, 0)->ty == TOKEN_TYPE_BANG)) {
-                Token *op = lexer_next(l);
-                Expr *operand = parse_primary_expr(l);
+        while (lexer_peek(ctx->l, 0) && (lexer_peek(ctx->l, 0)->ty == TOKEN_TYPE_MINUS
+                                 || lexer_peek(ctx->l, 0)->ty == TOKEN_TYPE_BANG)) {
+                Token *op = lexer_next(ctx->l);
+                Expr *operand = parse_primary_expr(ctx);
                 left = (Expr *)expr_unary_alloc(operand, op->lx.s, op->lx.l);
         }
 
         while (1) {
-                Token *hd = lexer_peek(l, 0);
+                Token *hd = lexer_peek(ctx->l, 0);
                 if (!hd) { return left; }
                 switch (hd->ty) {
                 case TOKEN_TYPE_IDENTIFIER: {
                         left = (Expr *)expr_identifier(hd->lx.s, hd->lx.l);
-                        lexer_discard(l); // identifier
+                        lexer_discard(ctx->l); // identifier
                 } break;
                 case TOKEN_TYPE_LPAREN: {
-                        (void)lexer_next(l); // (
-                        Expr_Array tuple = parse_comma_sep_exprs(l);
-                        (void)expect(l, TOKEN_TYPE_RPAREN);
+                        (void)lexer_next(ctx->l); // (
+                        Expr_Array tuple = parse_comma_sep_exprs(ctx);
+                        (void)expect(ctx->l, TOKEN_TYPE_RPAREN);
                         if (tuple.len > 1) {
                                 // Tuple
                                 //left = expr_tuple_alloc(tuple, tok);
@@ -79,14 +84,14 @@ static Expr *parse_primary_expr(Lexer *l) {
                         }
                 } break;
                 case TOKEN_TYPE_INTLIT: {
-                        Token *intlit = lexer_next(l);
+                        Token *intlit = lexer_next(ctx->l);
                         assert(intlit->lx.l < 32);
                         char buf[32] = {0};
                         (void)strncpy(buf, intlit->lx.s, intlit->lx.l);
                         left = (Expr *)expr_intlit_alloc(atoi(buf));
                 } break;
                 case TOKEN_TYPE_STRLIT: {
-                        Token *strlit = lexer_next(l);
+                        Token *strlit = lexer_next(ctx->l);
                         left = (Expr *)expr_strlit_alloc(strlit->lx.s, strlit->lx.l);
                 } break;
                 case TOKEN_TYPE_KEYWORD: {
@@ -99,7 +104,7 @@ static Expr *parse_primary_expr(Lexer *l) {
                         }
                 } break;
                 /* case TOKEN_TYPE_NEWLINE: { */
-                /*         lexer_discard(l); // \n */
+                /*         lexer_discard(ctx->l); // \n */
                 /*         if (!left) { */
                 /*                 err("could not parse primary expression, hit newline"); */
                 /*         } */
@@ -113,83 +118,87 @@ static Expr *parse_primary_expr(Lexer *l) {
         return left;
 }
 
-static Expr *parse_multiplicative_expr(Lexer *l) {
-    Expr *lhs = parse_primary_expr(l);
-    Token *cur = lexer_peek(l, 0);
+static Expr *parse_multiplicative_expr(Parsing_Context *ctx) {
+    Expr *lhs = parse_primary_expr(ctx);
+    Token *cur = lexer_peek(ctx->l, 0);
     while (cur && (cur->ty == TOKEN_TYPE_ASTERISK
                    || cur->ty == TOKEN_TYPE_FORWARD_SLASH
                    || cur->ty == TOKEN_TYPE_PERCENT)) {
-        Token* op = lexer_next(l);
-        Expr *rhs = parse_primary_expr(l);
+        Token* op = lexer_next(ctx->l);
+        Expr *rhs = parse_primary_expr(ctx);
         lhs = (Expr *)expr_binary_alloc(lhs, rhs, op->lx.s, op->lx.l);
-        cur = lexer_peek(l, 0);
+        cur = lexer_peek(ctx->l, 0);
     }
     return lhs;
 }
 
-static Expr *parse_additive_expr(Lexer *l) {
-    Expr *lhs = parse_multiplicative_expr(l);
-    Token *cur = lexer_peek(l, 0);
+static Expr *parse_additive_expr(Parsing_Context *ctx) {
+    Expr *lhs = parse_multiplicative_expr(ctx);
+    Token *cur = lexer_peek(ctx->l, 0);
     while (cur && (cur->ty == TOKEN_TYPE_PLUS
                    || cur->ty == TOKEN_TYPE_MINUS)) {
-        Token *op = lexer_next(l);
-        Expr *rhs = parse_multiplicative_expr(l);
+        Token *op = lexer_next(ctx->l);
+        Expr *rhs = parse_multiplicative_expr(ctx);
         lhs = (Expr *)expr_binary_alloc(lhs, rhs, op->lx.s, op->lx.l);
-        cur = lexer_peek(l, 0);
+        cur = lexer_peek(ctx->l, 0);
     }
     return lhs;
 }
 
-static Expr *parse_equalitive_expr(Lexer *l) {
-    Expr *lhs = parse_additive_expr(l);
-    Token *cur = lexer_peek(l, 0);
+static Expr *parse_equalitive_expr(Parsing_Context *ctx) {
+    Expr *lhs = parse_additive_expr(ctx);
+    Token *cur = lexer_peek(ctx->l, 0);
     while (cur && (cur->ty == TOKEN_TYPE_DOUBLE_EQUALS
                    || cur->ty == TOKEN_TYPE_GREATER_THAN_EQUALS
                    || cur->ty == TOKEN_TYPE_GREATER_THAN
                    || cur->ty == TOKEN_TYPE_LESS_THAN_EQUALS
                    || cur->ty == TOKEN_TYPE_LESS_THAN
                    || cur->ty == TOKEN_TYPE_BANG_EQUALS)) {
-        Token *op = lexer_next(l);
-        Expr *rhs = parse_additive_expr(l);
+        Token *op = lexer_next(ctx->l);
+        Expr *rhs = parse_additive_expr(ctx);
         lhs = (Expr *)expr_binary_alloc(lhs, rhs, op->lx.s, op->lx.l);
-        cur = lexer_peek(l, 0);
+        cur = lexer_peek(ctx->l, 0);
     }
     return lhs;
 }
 
-static Expr *parse_logical_expr(Lexer *l) {
-    Expr *lhs = parse_equalitive_expr(l);
-    Token *cur = lexer_peek(l, 0);
+static Expr *parse_logical_expr(Parsing_Context *ctx) {
+    Expr *lhs = parse_equalitive_expr(ctx);
+    Token *cur = lexer_peek(ctx->l, 0);
     while (cur && (cur->ty == TOKEN_TYPE_DOUBLE_AMPERSAND
                    || cur->ty == TOKEN_TYPE_DOUBLE_PIPE)) {
-        Token *op = lexer_next(l);
-        Expr *rhs = parse_equalitive_expr(l);
+        Token *op = lexer_next(ctx->l);
+        Expr *rhs = parse_equalitive_expr(ctx);
         lhs = (Expr *)expr_binary_alloc(lhs, rhs, op->lx.s, op->lx.l);
-        cur = lexer_peek(l, 0);
+        cur = lexer_peek(ctx->l, 0);
     }
     return lhs;
 }
 
-static Expr *parse_bitwise_expr(Lexer *l) {
-        return parse_logical_expr(l);
+static Expr *parse_bitwise_expr(Parsing_Context *ctx) {
+        return parse_logical_expr(ctx);
 }
 
-static Expr *parse_expr(Lexer *l) {
-        Token *hd = lexer_peek(l, 0);
+static Expr *parse_expr(Parsing_Context *ctx) {
+        Token *hd = lexer_peek(ctx->l, 0);
         if (!strncmp(hd->lx.s, GL_KW_LET, hd->lx.l)) {
-                return (Expr *)parse_expr_let(l);
+                return (Expr *)parse_expr_let(ctx);
         }
-        Expr *e = parse_bitwise_expr(l);
+        Expr *e = parse_bitwise_expr(ctx);
         assert(e);
         return e;
 }
 
 Program parser_parse_program(Lexer *l) {
-        Program p = (Program) {
-                .exprs = dyn_array_empty(Expr_Array),
+        Program p = (Program) { .exprs = dyn_array_empty(Expr_Array) };
+
+        Parsing_Context ctx = (Parsing_Context) {
+                .l = l,
+                .global_scope = 0,
         };
-        while (lexer_speek(l, 0)->ty != TOKEN_TYPE_EOF) {
-                dyn_array_append(p.exprs, parse_expr(l));
+
+        while (lexer_speek(ctx.l, 0)->ty != TOKEN_TYPE_EOF) {
+                dyn_array_append(p.exprs, parse_expr(&ctx));
         }
         return p;
 }
